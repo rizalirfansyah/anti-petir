18652b15797cba574b41864fdfe8fa29c41565c6
Rizal Irfansyah <rizalfriendchicken@student.ittelkom-sby.ac.id>
2023-03-14 10:16:27 +0700

    carousel but not fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,84 +72,88 @@
         <div class="absolute animate__animated animate__fadeInUp top-72 w-3/5 h-12 ml-12 lg:ml-20 text-white">
             <h2 class="text-4xl font-bold">Penangkal Petir - Anti Petir</h2>
         </div>
-        <div class="absolute animate__animated animate__fadeInLeft top-[360px] lg:top-80 w-[420px] h-12 ml-12 lg:ml-20 text-white">
+        <div class="absolute wow animate__fadeInLeft top-[360px] lg:top-80 w-[420px] h-12 ml-12 lg:ml-20 text-white">
             <h6 class="text-lg mt-6">Dewata Technoindo© menyediakan pemasangan Penangkal Petir NeoFLASH jenis Elektrostatik dan Arrester melayani instalasi se-Indonesia beserta kelengkapan material</h6>
         </div>
     </div>
 
     {{-- Carousel --}}
-    <div id="carouselExampleCrossfade" class="relative" data-te-carousel-init data-te-carousel-slide>
-    <div class="absolute inset-x-0 bottom-0 z-[2] mx-[15%] mb-4 flex list-none justify-center p-0" data-te-carousel-indicators>
-        <button type="button" data-te-target="#carouselExampleCrossfade" data-te-slide-to="0" data-te-carousel-active class="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-te-target="#carouselExampleCrossfade" data-te-slide-to="1" class="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none" aria-label="Slide 2"></button>
-    </div>
-    <div class="relative w-full h-[500px] overflow-hidden after:clear-both after:block after:content-['']">
-        <div class="relative float-left -mr-[100%] w-full !transform-none opacity-0 transition-opacity duration-[600ms] ease-in-out motion-reduce:transition-none" data-te-carousel-fade data-te-carousel-item data-te-carousel-active>
-        <img
-            src="img/bg/cable.jpg"
-            class="block w-full"
-            alt="Wild Landscape" />
-            <div
-            class="absolute top-40 inset-x-[15%] bottom-5 hidden py-5 text-center text-white md:block">
-            <h5 class="text-xl">Testing</h5>
-            <a href="" class="text-gray-200 text-xl animate__animated animate_fadeInLeft ">
-                NYY Sebagai Kabel Penyalur Petir
-            </a>
-          </div>
+        <div id="carouselExampleCrossfade" class="relative" data-te-carousel-init data-te-carousel-slide>
+        <div class="absolute inset-x-0 bottom-0 z-[2] mx-[15%] mb-4 flex list-none justify-center p-0" data-te-carousel-indicators>
+            <button type="button" data-te-target="#carouselExampleCrossfade" data-te-slide-to="0" data-te-carousel-active class="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-te-target="#carouselExampleCrossfade" data-te-slide-to="1" class="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none" aria-label="Slide 2"></button>
         </div>
-        <div class="relative float-left -mr-[100%] hidden w-full !transform-none opacity-0 transition-opacity duration-[600ms] ease-in-out motion-reduce:transition-none" data-te-carousel-fade data-te-carousel-item>
-        <img
-            src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
-            class="block w-full"
-            alt="Camera" />
+        <div class="relative w-full h-[500px] overflow-hidden after:clear-both after:block after:content-['']">
+            <div class="relative float-left -mr-[100%] w-full !transform-none opacity-0 transition-opacity duration-[600ms] ease-in-out motion-reduce:transition-none" data-te-carousel-fade data-te-carousel-item data-te-carousel-active>
+            <img
+                src="img/bg/cable.jpg"
+                class="block w-full"
+                alt="Wild Landscape" />
+                <div class="absolute top-40 inset-x-[15%] bottom-5 hidden py-5 text-white md:block wow slideInRight">
+                <a href="" class="text-gray-100 text-sm font-bold px-1 border-md rounded-sm bg-red-900">KABEL PENYALUR PETIR</a>
+                <h4 class="text-white absolute top-5 ml-44">Oktober 18, 2014 - by <strong>abah petir</strong></h4>
+                <h1 class="text-gray-200 text-3xl wow animate__fadeInBottomRight animate__slow">
+                    NYY Sebagai Kabel Penyalur Petir
+                </h2>
+            </div>
+            </div>
+            <div class="relative float-left -mr-[100%] hidden w-full !transform-none opacity-0 transition-opacity duration-[600ms] ease-in-out motion-reduce:transition-none" data-te-carousel-fade data-te-carousel-item>
+            <img
+                src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
+                class="block w-full"
+                alt="Camera" />
+            </div>
         </div>
-    </div>
-    <button
-        class="absolute top-0 bottom-0 left-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
-        type="button"
-        data-te-target="#carouselExampleCrossfade"
-        data-te-slide="prev">
-        <span class="inline-block h-8 w-8">
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="h-6 w-6">
-            <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M15.75 19.5L8.25 12l7.5-7.5" />
-        </svg>
-        </span>
-        <span
-        class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-        >Previous</span
-        >
-    </button>
-    <button
-        class="absolute top-0 bottom-0 right-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
-        type="button"
-        data-te-target="#carouselExampleCrossfade"
-        data-te-slide="next">
-        <span class="inline-block h-8 w-8">
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="h-6 w-6">
-            <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-        </svg>
-        </span>
-        <span
-        class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-        >Next</span
-        >
-    </button>
+        <button
+            class="absolute top-0 bottom-0 left-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
+            type="button"
+            data-te-target="#carouselExampleCrossfade"
+            data-te-slide="prev">
+            <span class="inline-block h-8 w-8">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="h-6 w-6">
+                <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.75 19.5L8.25 12l7.5-7.5" />
+            </svg>
+            </span>
+            <span
+            class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+            >Previous</span
+            >
+        </button>
+        <button
+            class="absolute top-0 bottom-0 right-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
+            type="button"
+            data-te-target="#carouselExampleCrossfade"
+            data-te-slide="next">
+            <span class="inline-block h-8 w-8">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="h-6 w-6">
+                <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+            </svg>
+            </span>
+            <span
+            class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+            >Next</span
+            >
+        </button>
+        {{-- <div class=" absolute h-96 max-w-full 2xl:w-full lg:w-full md:w-full sm:max-w-full bg-no-repeat overflow-hidden mx-auto bg-green-200">
+            <h2 class="absolute text-center mt-24 wow fadeInRight">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui id sunt eligendi praesentium perspiciatis beatae nesciunt deserunt eum! Reiciendis quae voluptatum aliquam pariatur atque. Libero non doloremque voluptate eum, sapiente labore animi optio suscipit error provident atque voluptatem dolores officia, illo aperiam, ad dolorum eaque consequuntur eos et dignissimos neque.</h2>
+        </div> --}}
+        
     </div>
