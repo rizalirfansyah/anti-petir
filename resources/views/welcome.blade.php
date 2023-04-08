aa73609d2a1e16f2d92d27d82e472b099ae59fa6
Rizal Irfansyah <rizalfriendchicken@student.ittelkom-sby.ac.id>
2023-04-08 09:30:21 +0700

    footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -262,6 +262,20 @@
     {{-- artikel end --}}
    
     {{-- footer --}}
-    <div class="bg-gray-700 h-12">
-        <div class="text-start text-gray-200 font-semibold text-xs py-4 xl:ml-56 lg:ml-52 md:ml-40 sm:ml-32 ml-20">© 2023 Antipetir.com / All rights reserved</div>
-    </div>
+    <footer class="bg-gray-50 rounded-lg shadow m-4 dark:bg-gray-800">
+        <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between  wow animate__fadeInUp">
+        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Anti-Petir</a>. All Rights Reserved.
+        </span>
+        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6">Testimoni</a>
+            </li>
+            <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6">Lisensi</a>
+            </li>
+            <li>
+                <a href="#" class="hover:underline">Tentang Kami</a>
+            </li>
+        </ul>
+        </div>
+    </footer>
