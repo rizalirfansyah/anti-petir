9ee3a2f57d5980d8e27f666fa938ef0a90f2c6db
Rizal Irfansyah <rizalfriendchicken@student.ittelkom-sby.ac.id>
2023-04-08 10:15:53 +0700

    fix footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -261,11 +261,11 @@
     {{-- artikel end --}}
     
     {{-- footer --}}    
-    <footer class="bg-gray-50 rounded-lg shadow m-4 dark:bg-gray-800">
+    <footer class="bg-slate-100 rounded-lg shadow m-4 dark:bg-gray-800">
         <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between  wow animate__fadeInUp">
-        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Anti-Petir</a>. All Rights Reserved.
+        <span class="text-sm text-gray-300 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Anti-Petir</a>. All Rights Reserved.
         </span>
-        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-300 dark:text-gray-100 sm:mt-0">
             <li>
                 <a href="#" class="mr-4 hover:underline md:mr-6">Testimoni</a>
             </li>
